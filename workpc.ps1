377c23129424e2e8b084f62ce3cc3ee932beee07
Jason Underhill <jay.underhill@gmail.com>
2018-01-22 10:54:04 +0000

    Update packages for new work pc install

--- workpc.ps1
+++ workpc.ps1
@@ -14,14 +14,27 @@ Disable-UAC
 # -------------------------------------------------
 
 choco install Firefox
+choco install office365proplus
 choco install git
 choco install javaruntime
-choco install atom
 choco install vim
-choco install putty
+choco install 7zip
+choco install f.lux
+choco install foobar2000
+choco install notepadplusplus
+choco install vlc
+choco install putty.install
 choco install dropbox
-choco install conemu
+choco install visualstudio2017community
 choco install resharper
 choco install visualstudiocode
-choco install mssqlservermanagementstudio2014express
+choco install sql-server-express
+choco install sql-server-management-studio
 choco install lastpass
+choco install nodejs
+choco install filezilla
+choco install sharex
+choco install virtualbox
+choco install babun
+
+
